62ea7172577e2115d1fd047ead9b7b579f3430a8
Michael Grewal <58837913+michaelgrewal@users.noreply.github.com>
2022-11-20 16:03:10 -0500

    test msg passing (#1)
    Co-authored-by: Michael Grewal <MichaelGrewal@Michaels-MacBook-Pro.local>

--- flight_controller.c
+++ flight_controller.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/neutrino.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+#include <string.h>
 
+#include "flight_controller.h"
+
+
+typedef union
+{
+	struct _pulse pulse;
+	char rmsg [MAX_STRING_LEN +1];
+} myMessage_t;
+
+
+int main(int argc, char* argv[])
+{
+	printf("[FC] Hi I'm FC Server\n");
+	int rcvid;
+	char reply_msg[MAX_STRING_LEN];
+	myMessage_t msg;
+
+	name_attach_t *attach;
+	attach = name_attach(NULL, SERVER_NAME, 0);
+
+	while(1)
+	{
+		rcvid = MsgReceive(attach->chid, &msg, sizeof(msg), NULL);
+
+		//pulse
+		if (rcvid == 0) {
+			switch (msg.pulse.code) {
+			case _PULSE_CODE_DISCONNECT:
+				printf("client is gone\n");
+				ConnectDetach(msg.pulse.scoid);
+				break;
+			}
+		}
+		//message
+		else {
+			printf("[FC] Received message: %s\n", msg.rmsg);
+			strcpy(reply_msg, "from server");
+			MsgReply(rcvid, EOK, &reply_msg, sizeof(reply_msg));
+		}
+
+		memset(reply_msg, 0, sizeof(reply_msg));
+		memset(msg.rmsg, 0, sizeof(msg.rmsg));
+	}
+	name_detach(attach, 0);
+
+    return EXIT_SUCCESS;
+}
--- flight_controller.h
+++ flight_controller.h
@@ -1,6 +1,8 @@
 #ifndef FLIGHT_CONTROLLER_H_
 #define FLIGHT_CONTROLLER_H_
 
+#define SERVER_NAME "flight_controller"
+#define MAX_STRING_LEN 64
 
 
 #endif /* FLIGHT_CONTROLLER_H_ */
