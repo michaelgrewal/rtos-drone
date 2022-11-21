546b79440c204ecb27e673d31af1ae0bca55c85b
Michael Grewal <58837913+michaelgrewal@users.noreply.github.com>
2022-11-20 19:22:12 -0500

    realtime inline print speeds (#5)
    Co-authored-by: Michael Grewal <MichaelGrewal@Michaels-MacBook-Pro.local>

--- display.c
+++ display.c
@@ -26,26 +26,13 @@ int main(int argc, char* argv[])
 	ptr = mmap(NULL, PAGE_SIZE, PROT_READ, MAP_SHARED, fd, 0);
 	close(fd);
 
-	printf("[D] Reads Speed1: %d, Speed2: %d, Speed3: %d, Speed4: %d\n", ((int*)ptr)[0], ((int*)ptr)[1], ((int*)ptr)[2], ((int*)ptr)[3]);
-
+	sleep(1);
+	printf("[D] Reads Speed1: %d, Speed2: %d, Speed3: %d, Speed4: %d", ((int*)ptr)[0], ((int*)ptr)[1], ((int*)ptr)[2], ((int*)ptr)[3]);
+	fflush(stdout);
 	sleep(3);
 
-	printf("[D] Reads Speed1: %d, Speed2: %d, Speed3: %d, Speed4: %d\n", ((int*)ptr)[0], ((int*)ptr)[1], ((int*)ptr)[2], ((int*)ptr)[3]);
-
-	//set the message value to 'test'
-//	strcpy(smsg, "test");
-
-	//establish a connection to the server's channel
-//	coid = name_open(SERVER_NAME, 0);
-
-	//send the message to the server and get a reply
-//	MsgSend(coid, &smsg, sizeof(smsg), &rmsg, sizeof(rmsg));
-
-    //print received checksum
-//	printf("[D] Received message: %s\n", rmsg);
-
-    //Close a server connection that was opened by name_open()
-//	name_close(coid);
+	printf("\r[D] Reads Speed1: %d, Speed2: %d, Speed3: %d, Speed4: %d", ((int*)ptr)[0], ((int*)ptr)[1], ((int*)ptr)[2], ((int*)ptr)[3]);
+	fflush(stdout);
 
     return EXIT_SUCCESS;
 }
