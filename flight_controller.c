def81c64764480f1370a486225132bd83f8a48bb
Michael Grewal <58837913+michaelgrewal@users.noreply.github.com>
2022-11-26 14:54:59 -0500

    altitude simulation with timeout (#14)
    Co-authored-by: Michael Grewal <MichaelGrewal@Michaels-MacBook-Pro.local>

--- flight_controller.c
+++ flight_controller.c
@@ -20,22 +20,33 @@ typedef union
 	set_target_speed_msg_t msg_set_target;
 } recv_buf_t;
 
+// Forward declarations
 int create_shared_memory(int nbytes, void **ptr);
 int calc_speed(direction_t direction, int curr_speed);
+void calculate_altitude(void *ptr);
+void setup_altitude_periodic_updates(pid_t server_pid, int chid, struct itimerspec *it, struct sigevent *se, timer_t *tID);
+
 
 int main(int argc, char* argv[])
 {
-//	printf("[FC] Hi I'm FC Server\n");
 	int rcvid;
 	int target_speed = HOVER;
 	char reply_msg[MAX_STRING_LEN];
 	recv_buf_t msg;
 	get_target_speed_resp_t resp_target;
 	void *ptr;
+	struct sigevent sigevent;
+	struct itimerspec itime;
+	timer_t timerID;
+	pid_t server_pid;
 
 	// register server name
 	name_attach_t *attach;
 	attach = name_attach(NULL, SERVER_NAME, 0);
+
+	// Timeout setup for periodic altitude updates
+	server_pid = getpid();
+	setup_altitude_periodic_updates(server_pid, attach->chid, &itime, &sigevent, &timerID);
 
 	// create shared memory object and return ptr
 	if (create_shared_memory(PAGE_SIZE, &ptr) != 0) {
@@ -48,6 +59,7 @@ int main(int argc, char* argv[])
 	((int*)ptr)[1] = 0;		// Propeller 2 speed
 	((int*)ptr)[2] = 0;		// Propeller 3 speed
 	((int*)ptr)[3] = 0;		// Propeller 4 speed
+	((int*)ptr)[4] = 0;		// Altitude
 
 	while(1)
 	{
@@ -75,6 +87,9 @@ int main(int argc, char* argv[])
 				// pulse from Sensor 4 giving Propeller 4 speed
 				case _PULSE_CODE_UPDATE_SPEED4:
 					((int*)ptr)[3] = msg.pulse.value.sival_int;
+					break;
+				case _PULSE_CODE_TIMER_ALTITUDE_UPDATE:
+					calculate_altitude(ptr);
 					break;
 				}
 		}
@@ -107,6 +122,44 @@ int main(int argc, char* argv[])
 	munmap(ptr, PAGE_SIZE);
 
     return EXIT_SUCCESS;
+}
+
+
+// Simulates altitude. Periodic check if altitude is going up or down
+void calculate_altitude(void *ptr) {
+	int speed1, speed2, speed3, speed4, altitude;
+	speed1 = ((int*)ptr)[0];
+	speed2 = ((int*)ptr)[1];
+	speed3 = ((int*)ptr)[2];
+	speed4 = ((int*)ptr)[3];
+	altitude = ((int*)ptr)[4];
+
+	// if all propellers are above HOVER then drone is ascending in altitude
+	if (speed1 > HOVER && speed2 > HOVER && speed3 > HOVER && speed4 > HOVER) {
+		altitude += ALTITUDE_RATE;
+	}
+	// else if all propellers are below HOVER then drone is descending in altitude
+	else if (speed1 < HOVER && speed2 < HOVER && speed3 < HOVER && speed4 < HOVER && altitude > 0) {
+		altitude -= ALTITUDE_RATE;
+	}
+	// otherwise drone is hovering and no change to altitude
+
+	((int*)ptr)[4] = altitude;
+}
+
+// Periodic timeout for pulse altitude updates
+void setup_altitude_periodic_updates(pid_t server_pid, int chid, struct itimerspec *it, struct sigevent *se, timer_t *tID) {
+	struct itimerspec itime = *it;
+	struct sigevent sigevent = *se;
+	timer_t timerID = *tID;
+	int coid;
+
+	coid = ConnectAttach(0, server_pid, chid, _NTO_SIDE_CHANNEL, 0);
+	SIGEV_PULSE_INIT(&sigevent, coid, 0, _PULSE_CODE_TIMER_ALTITUDE_UPDATE, 0);
+	timer_create(CLOCK_REALTIME, &sigevent, &timerID);
+	itime.it_value.tv_sec = 2;
+	itime.it_interval.tv_sec = 2;
+	timer_settime(timerID, 0, &itime, NULL);
 }
 
 // creates shared memory to store all propeller speed data
